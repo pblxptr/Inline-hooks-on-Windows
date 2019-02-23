6b472a52704f642b889a8b738365625c41015f0a
bielu000 <bielu000@o2.pl>
2019-02-23 08:50:01 +0100

    Initial commit

---
+++ Injector/Injcector.cpp
@@ -0,0 +1,94 @@
+#include <Windows.h>
+#include <stdio.h>
+
+#define PID_EMPTY 0
+
+DWORD init_process(const char* app)
+{
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
+	
+	ZeroMemory(&si, sizeof(si));
+	ZeroMemory(&pi, sizeof(pi));
+
+	si.cb = sizeof(pi);
+
+	char ap[] = "sdasd";
+
+	if (!CreateProcess(NULL, (char*)app, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return 0;
+	}
+
+	//CloseHandle ?? 
+
+	return pi.dwProcessId;
+}
+
+int main()
+{
+	char hookLibname[] = "hook.dll";
+	//LoadLibraryA("hook.dll");
+	DWORD processId = init_process("SimpleWS.exe");
+
+	if (processId == PID_EMPTY) {
+		puts("Pid empty.");
+		return 1;
+	}
+
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+
+	if (hProcess == NULL) {
+		puts("hProcess is null.");
+
+		return 1;
+	}
+	puts("Proces opened.");
+
+	//Allocate space for libname
+	LPVOID libnameAddr = VirtualAllocEx(hProcess, NULL, 0x1000, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	if (libnameAddr == NULL) {
+		puts("VirtualAllocEx error.");
+
+		CloseHandle(hProcess);
+
+		return 1;
+	}
+	puts("Memory allocated");
+
+	//Copy lib name into remote(target) space
+	SIZE_T bytesWritten;
+	if (!WriteProcessMemory(hProcess, libnameAddr, hookLibname, strlen(hookLibname) + 1, &bytesWritten)) {
+		puts("WriteProcessMemory error.");
+
+		CloseHandle(hProcess);
+	}
+	puts("Library name copied.");
+
+	//Load library in remote process by creating new remote thread
+	LPTHREAD_START_ROUTINE routine = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
+
+	getchar();
+
+	DWORD threadId;
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, routine, libnameAddr, 0, &threadId);
+
+	if (hThread == NULL) {
+		puts("CreateRemoteThread error.");
+
+		CloseHandle(hProcess);
+	}
+	puts("Thread created -> Library should be loaded.");
+
+	puts("Press any key to exit...");
+	getchar();
+
+	//CloseHandle(hThread);
+	//CloseHandle(hProcess);
+	
+	TerminateProcess(hProcess, 1);
+
+	
+}
---
+++ SimpleWS/server.cpp
@@ -0,0 +1,109 @@
+#include <WinSock2.h>
+#include <stdio.h>
+#include <ws2tcpip.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+void error(const char* msg)
+{
+	printf("Error: %s\n", msg);
+
+	getchar();
+
+	ExitProcess(EXIT_FAILURE);
+}
+
+int main()
+{
+	WSADATA wsdata;
+	int iResult;
+
+	getchar();
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsdata);
+
+	if (iResult != NO_ERROR) {
+		error("WSAStartup");
+	}
+
+	SOCKET s;
+	
+	s = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (s == INVALID_SOCKET) {
+		error("socket");
+	}
+
+	printf("Socket: %d\n", s);
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+
+	struct addrinfo* res = NULL;
+	struct addrinfo* p = NULL;
+	if (getaddrinfo("127.0.0.1", "3490", &hints, &res) != 0) {
+		error("getaddrinfo");
+	}
+
+	char ipaddress[INET6_ADDRSTRLEN];
+	short port;
+
+	for (p = res ; p != NULL; p = p->ai_next) {
+		void* addr = NULL;
+		const char *ipver;
+
+		if (p->ai_family == AF_INET) {
+			struct sockaddr_in* ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+			inet_ntop(p->ai_family, addr, (PSTR)ipaddress, sizeof(ipaddress));
+		}
+
+		break;
+	}
+
+	if (bind(s, (SOCKADDR*)p->ai_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
+		printf("%d", GetLastError());
+		error("bind");
+	}
+
+	if (listen(s, 10) != 0) {
+		error("listen");
+	}
+
+	
+	port = ntohs(((struct sockaddr_in*)p->ai_addr)->sin_port);
+
+
+	printf("IP: %s Port: %d\n", ipaddress, port);
+
+	SOCKET new_s;
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(sockaddr_in);
+
+	new_s = accept(s, (struct sockaddr *)&client_addr, &client_addr_len);
+	printf("accepted!\n");
+	
+	char client_address[INET6_ADDRSTRLEN];
+	inet_ntop(client_addr.sin_family, (void *)&client_addr.sin_addr, client_address, client_addr_len);
+
+	printf("Client IP address: %s\n", client_address);
+
+	char msg[] = "Hello World";
+	SIZE_T msg_len = strlen(msg) + 1;
+	SIZE_T sent_bytes = 0;
+
+	while (sent_bytes != msg_len) {
+		sent_bytes = send(new_s, msg + sent_bytes, msg_len - sent_bytes, 0);
+	}
+	printf("Sent!\n");
+
+	char buffer[512];
+	while (recv(new_s, buffer, sizeof(buffer), 0) != 0);
+	printf("Reveived\n");
+	
+
+	return 1;
+}
